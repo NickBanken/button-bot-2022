0752e2240e5a164a3ee62e749b3f38237ba0ae07
Bert Heyman <bert.heyman@hotmail.com>
2022-01-10 11:51:24 +0100

    Split core button bot functionality into smaller functions

--- Discord/buttonBot.js
+++ Discord/buttonBot.js
@@ -7,77 +7,79 @@ const { promotions } = require("./promotions");
 const { testServers } = require("./test-servers");
 const { buildMessage } = require("./generateMessage");
 
-router.get("/", function(req, res, next) {
+const sendMessageToGroup = (group, channel) => {
+    const discordChannel = group.channel_id;
+    const role = group.role_id;
+    const holidays = group.holidays;
 
+    if (discordChannel === channel.id.toString()) {
+        const notAHoliday = [];
+        const currentDate = new Date();
+
+        holidays.forEach(holiday => {
+            if (
+                currentDate >= holiday.start &&
+                currentDate.setUTCHours(23, 59, 59) <= holiday.end
+            ) {
+                notAHoliday.push(false);
+            }
+        });
+
+        if (
+            notAHoliday.length === 0 &&
+            (currentDate >= group.start &&
+                currentDate.setUTCHours(23, 59, 59) <= group.end)
+        ) {
+            const unfilteredMessage = buildMessage(role);
+            channel.send(unfilteredMessage);
+        }
+    }
+}
+
+// Get the generateMessage
+const composeMessage = guilds => {
+    guilds.map((guild, key) => {
+        // All channels on all Discord servers
+        guild.channels.cache.map(channel => {
+            promotions.forEach((promo, index) => {
+                sendMessageToGroup(promo, channel)
+            });
+        });
+    });
+};
+
+const buttonBotLoaded = () => {
+    console.log("READY!");
+    const guilds = client.guilds.cache.map(guild => guild);
+
+    composeMessage(guilds);
+
+    const date = new Date().toLocaleString("en-US", {
+        timeZone: "Europe/Brussels"
+    });
+
+    const localDate = new Date(date);
+    const localTime = localDate.getHours() + ":" + localDate.getMinutes();
+
+    console.log(localTime);
+}
+
+const buttonBot = (request, response, next) => {
     // Create a new client instance
     const client = new Client({
         intents: [Intents.FLAGS.GUILDS],
         allowedMentions: { parse: ["users", "roles"] }
     });
-    // Get the generateMessage
-    const composeMessage = guilds => {
-        let discordChannel;
-        let role;
-        let holidays;
 
-        guilds.map((guild, key) => {
-            // All channels on all Discord servers
-            guild.channels.cache.map(channel => {
-
-                promotions.forEach((promo, index) => {
-                    discordChannel = promo.channel_id;
-                    role = promo.role_id;
-                    holidays = promo.holidays;
-
-                    // All relevant promotions
-                    if (discordChannel === channel.id.toString()) {
-                        const notAHoliday = [];
-                        const currentDate = new Date();
-
-                        holidays.forEach(holiday => {
-                            if (
-                                currentDate >= holiday.start &&
-                                currentDate.setUTCHours(23, 59, 59) <= holiday.end
-                            ) {
-                                notAHoliday.push(false);
-                            }
-                        });
-
-                        if (
-                            notAHoliday.length === 0 &&
-                            (currentDate >= promo.start &&
-                                currentDate.setUTCHours(23, 59, 59) <= promo.end)
-                        ) {
-                            const unfilteredMessage = buildMessage(role);
-                            channel.send(unfilteredMessage);
-                        }
-                    }
-                });
-            });
-        });
-    };
-
-    client.once("ready", () => {
-        console.log("READY!");
-        const guilds = client.guilds.cache.map(guild => guild);
-
-        composeMessage(guilds);
-
-        const date = new Date().toLocaleString("en-US", {
-            timeZone: "Europe/Brussels"
-        });
-
-        const localDate = new Date(date);
-        const localTime = localDate.getHours() + ":" + localDate.getMinutes();
-
-        console.log(localTime);
-    });
+    client.once("ready", buttonBotLoaded);
 
     client.login(token);
 
     // Login to Discord with your client's token
 
-    res.render('index', { status: "Active", version: "1.0.0" })
-});
+    response.render('index', { status: "Active", version: "1.0.0" })
+}
+
+router.get("/", buttonBot(request, response, next));
 
 module.exports = router;
